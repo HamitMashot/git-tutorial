d6d2220040e98b34c1b4766d225b217976574413
HamitMashot <haptohta@gmail.com>
2021-01-09 00:45:59 -0800

    Add 1 row rorint hello go  --hmt,1row hello world ,1row sum function

--- hello.go
+++ hello.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Go!!!!!!!!!!!!!!!")
-	fmt.Println("Hello Go!!!!!!!!!!!!!!!")
-	fmt.Println("Hello Go!!!!!!!!!!!!!!!")
-	fmt.Println("Hello Go!!!!!!!!!!!!!!!")
+	fmt.Println("Hello Go----HMT")
+	fmt.Println("Hello World!!!")
+	var A = 2
+	var B = 3
+	var sum = A + B
+	fmt.Println("sum", sum)
+
 }
